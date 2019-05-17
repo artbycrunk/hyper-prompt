8fb8b454f177e54d574179089a4f5937bc0b4ab3
Savio Fernandes <artbycrunk@gmail.com>
2019-05-17 21:06:17 +0700

    add symbol for hostname and root

--- hyper_prompt/segments/hostname.py
+++ hyper_prompt/segments/hostname.py
@@ -5,6 +5,8 @@ from ..segment import BasicSegment
 
 
 class Segment(BasicSegment):
+    SYMBOL = u'\uf6c3'
+
     @property
     def hostname(self):
         if not hasattr(self, "_hostname"):
@@ -13,10 +15,13 @@ class Segment(BasicSegment):
 
     def activate(self):
         host_prompt = self.hyper_prompt.shell_vars.get(
-            "hostname", " %s " % self.hostname.split(".")[0])
+            "hostname", self.hostname.split(".")[0])
+
+        content = self.symbol('hostname') + host_prompt
+
         if self.seg_conf.get("colorize"):
             FG, BG = colors.string_to_colors(self.hostname, short=True)
         else:
             FG = self.theme.get("HOSTNAME_FG", 250)
             BG = self.theme.get("HOSTNAME_BG", 238)
-        self.append(host_prompt, FG, BG)
+        self.append(self.hyper_prompt._content % (content), FG, BG)
--- hyper_prompt/segments/root.py
+++ hyper_prompt/segments/root.py
@@ -3,10 +3,13 @@ from ..segment import BasicSegment
 
 class Segment(BasicSegment):
     def activate(self):
+        root = self.hyper_prompt.shell_vars.get("root", '')
+        content = self.symbol('root') + root
+
         bg = self.theme.get("CMD_PASSED_BG", 236)
         fg = self.theme.get("CMD_PASSED_FG", 15)
         if self.hyper_prompt.args.prev_error != 0:
             fg = self.theme.get("CMD_FAILED_FG", 15)
             bg = self.theme.get("CMD_FAILED_BG", 161)
-        self.append(self.hyper_prompt.shell_vars.get("root", ''),
+        self.append(self.hyper_prompt._content % (content),
                     fg, bg, sanitize=False)
