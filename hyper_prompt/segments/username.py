f59166799405297761f63bc42cf88ec7f77f39cc
Savio Fernandes <artbycrunk@gmail.com>
2019-05-28 22:46:27 +0700

    add username symbol

--- hyper_prompt/segments/username.py
+++ hyper_prompt/segments/username.py
@@ -3,16 +3,20 @@ import getpass
 
 
 class Segment(BasicSegment):
+    SYMBOL = u'\uf2c0'
+
     def is_root(self):
         return getpass.getuser() == "root"
 
     def activate(self):
         user_prompt = self.hyper_prompt.shell_vars.get(
-            "username", " %s " % self.getenv("USER"))
+            "username", self.getenv("USER"))
+
+        content = self.symbol('username') + user_prompt
 
         fgcolor = self.theme.get("USERNAME_FG", 250)
         bgcolor = (self.theme.get("USERNAME_ROOT_BG", 124) if
                    self.is_root() else
                    self.theme.get("USERNAME_BG", 240))
 
-        self.append(user_prompt, fgcolor, bgcolor)
+        self.append(self.hyper_prompt._content % (content), fgcolor, bgcolor)
