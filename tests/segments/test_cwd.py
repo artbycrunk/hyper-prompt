a9c18d2a4c9bfe75c130c85ccfb9389636de6a68
Savio Fernandes <artbycrunk@gmail.com>
2019-12-11 22:19:38 +0700

    cleanup cwd test

--- tests/segments/test_cwd.py
+++ tests/segments/test_cwd.py
@@ -11,46 +11,62 @@ args = {"prev_error": 0, "shell": "bash"}
 ELLIPSIS = cwd.Segment.symbols.get("ellipsis", "\u2026")
 
 
-@pytest.fixture
-def cwd_segment():
+@pytest.fixture(name="cwd_segment")
+def fixture_cwd_segment():
     _importer = helpers.Importer()
     theme = _importer.import_theme("hyper_prompt.themes.default")
     hyper_prompt = Prompt(args, {}, theme)
     return cwd.Segment(hyper_prompt, {})
 
 
-def test_cwd_home(cwd_segment):
-    cwd_segment.hyper_prompt.cwd = os.getenv("HOME")
+@pytest.mark.parametrize(
+    "current_cwd, result, symbols",
+    [
+        (os.getenv("HOME"), "~", False),
+        (os.getenv("HOME"), "~", True),
+        ("/", "/", False),
+        ("/", "/", True),
+    ],
+    ids=["home-plain", "home-symbols", "root-plain", "root-symbols"],
+)
+def test_cwd(cwd_segment, current_cwd, result, symbols):
+    cwd_segment.hyper_prompt.cwd = current_cwd
+    symbol = ""
+    if symbols:
+        cwd_segment.hyper_prompt.show_symbols = True
+        symbol_name = "cwd_home" if result == "~" else "cwd"
+        symbol = cwd_segment.symbol(symbol_name, cwd_segment.symbols)
     cwd_segment.activate()
-    assert cwd_segment.content == defaults.CONTENT % "~"
+    assert cwd_segment.content == defaults.CONTENT % (symbol + result)
 
-def test_cwd_root(cwd_segment):
-    cwd_segment.hyper_prompt.cwd = "/"
-    cwd_segment.activate()
-    assert cwd_segment.content == defaults.CONTENT % "/"
 
 @pytest.mark.parametrize(
-    "cwd, result, mode",
+    "current_cwd, result, mode",
     [
         ("/var/tmp/hyper_prompt", "hyper_prompt", "dironly"),
         ("/var/tmp/hyper_prompt", "/var/tmp/hyper_prompt", "plain"),
     ],
     ids=["dironly", "plain"],
 )
-def test_cwd_mode(cwd_segment, cwd, result, mode):
-    cwd_segment.hyper_prompt.cwd = cwd
+def test_cwd_mode(cwd_segment, current_cwd, result, mode):
+    cwd_segment.hyper_prompt.cwd = current_cwd
     cwd_segment.seg_conf["mode"] = mode
     cwd_segment.setattrs()
     cwd_segment.activate()
     assert cwd_segment.content == defaults.CONTENT % result
 
 
-def test_cwd_maxsize(cwd_segment):
-    cwd_segment.hyper_prompt.cwd = "/var/tmp/hyper_prompt"
-    cwd_segment.seg_conf["max_dir_size"] = 5
+@pytest.mark.parametrize(
+    "current_cwd, result, max_dir_size",
+    [("/var/tmp/hyper_prompt", "/var/tmp/hyper", 5)],
+    ids=["size=5"],
+)
+def test_cwd_maxsize(cwd_segment, current_cwd, result, max_dir_size):
+    cwd_segment.hyper_prompt.cwd = current_cwd
+    cwd_segment.seg_conf["max_dir_size"] = max_dir_size
     cwd_segment.setattrs()
     cwd_segment.activate()
-    assert cwd_segment.content == defaults.CONTENT % "/var/tmp/hyper"
+    assert cwd_segment.content == defaults.CONTENT % result
 
 
 def test_cwd_readonly(cwd_segment):
@@ -62,7 +78,7 @@ def test_cwd_readonly(cwd_segment):
 
 
 @pytest.mark.parametrize(
-    "cwd, result, depth",
+    "current_cwd, result, depth",
     [
         ("/var/tmp/hyper_prompt", "/var/%s/hyper_prompt" % ELLIPSIS, 2),
         (
@@ -73,8 +89,8 @@ def test_cwd_readonly(cwd_segment):
     ],
     ids=["depth=2", "depth=3"],
 )
-def test_cwd_maxdepth(cwd_segment, cwd, result, depth):
-    cwd_segment.hyper_prompt.cwd = cwd
+def test_cwd_maxdepth(cwd_segment, current_cwd, result, depth):
+    cwd_segment.hyper_prompt.cwd = current_cwd
     cwd_segment.seg_conf["max_depth"] = depth
     cwd_segment.setattrs()
     cwd_segment.activate()
